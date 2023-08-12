7920ddc488d321cd73702c9e99923007eff0d8b9
Tazan <vincentoralca@gmail.com>
2023-08-12 12:57:24 +0900

    2023/8/12

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from "./component/login";
 function App() {
+  const style = {
+    marginTop: '150px', // You can adjust the value as needed
+  };
   return (
-    <div className="App">
-<SignIn />
+    <div className="App" style={style}>
+      <SignIn />
     </div>
   );
 }
--- src/component/login.js
+++ src/component/login.js
@@ -1,83 +1,211 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import myImage from '../assets/img/images.png';
+import '../assets/css/style.css';
+import React, { useState } from 'react';
 
 export default function SignIn() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
+
+    const [status, setStatus] = useState('otp');
+    const [carNumber, setCarNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [otp, setOtp] = useState('');
+
+    const handleCarNumberSubmit = (e) => {
+        e.preventDefault();
+        console.log(carNumber);
     };
 
+    const carNum = (event) => {
+        setCarNumber(event.target.value)
+    }
+
+
+    const handlePhoneNumberSubmit = (e) => {
+        e.preventDefault();
+        // Perform validation or API call for phone number
+        setStatus('otp');
+    };
+
+    const handleOtpSubmit = (e) => {
+        e.preventDefault();
+        // Perform validation or API call for OTP
+        // Complete login process
+    };
+
+    const boardcolor = {
+        backgroundColor: "#EBEBEB",
+        paddingTop: "15px",
+        paddingBottom: "50px"
+    }
+    console.log(status);
     return (
-        <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+        <Container component="main" maxWidth="xs" style={boardcolor}>
+            <div>
+                {status === 'carNumber' && (
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
                     >
-                        Sign In
-                    </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
+                        <Typography component="h1" variant="h5" sx={{ marginBottom: '20px' }}  >
+                            Logga in
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '17px' }}>
+                            Fyll i mobilenummer sa skickar vi en kod vis SMS
+                        </Typography>
+
+                        <Box component="form" onSubmit={handleCarNumberSubmit} noValidate sx={{ mt: 1 }} style={{ marginTop: "45px" }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Car Register No"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={carNum}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, backgroundColor: '#9F9F9F', color: 'white', padding: '8px', }}
+                                endIcon={<ArrowForwardIcon />}
+                                style={{ fontSize: '17px ' }}
+                            >
+                                Sticka code
+                            </Button>
+
+                        </Box>
+                        <Box style={{ marginTop: "40px" }}>
+                            <img src={myImage} className="image-style" alt="Description of the image" />
+                            <Typography variant="body1" component="p" sx={{ fontSize: '20px', padding: "40px  " }}>
+                                Har du Fragor om service eller verkstad?
+                            </Typography>
+                        </Box>
+                    </Box>
+                )}
+                {status === 'phoneNumber' && (
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Typography component="h1" variant="h5" sx={{ marginBottom: '20px' }}  >
+                            Logga in
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '17px' }}>
+                            Fyll i mobilenummer sa skickar vi en kod vis SMS
+                        </Typography>
+
+                        <Box component="form" onSubmit={handlePhoneNumberSubmit} noValidate sx={{ mt: 1 }} style={{ marginTop: "35px" }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="ABC 123"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, backgroundColor: '#9F9F9F', color: 'white', padding: '8px', }}
+                                endIcon={<ArrowForwardIcon />}
+                                style={{ fontSize: '17px ' }}
+                            >
+                                Sticka kod
+                            </Button>
+                        </Box>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '18px', marginTop: '15px', color: '#5d5d5d' }}>
+                            This is your phone Number
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '18px', color: "red" }}>
+                            1543213215645642
+                        </Typography>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, backgroundColor: '#12d867', color: 'white', padding: '8px', width: "330px" }}
+                            style={{ fontSize: '17px ' }}
+                        >
+                            Confirm
+                        </Button>
+                        <Box style={{ marginTop: "15px" }}>
+                            <img src={myImage} className="image-style" alt="Description of the image" />
+                            <Typography variant="body1" component="p" sx={{ fontSize: '20px', padding: "20px  ", color: '#5d5d5d' }}>
+                                Har du Fragor om service eller verkstad?
+                            </Typography>
+                        </Box>
+                    </Box>
+                )}
+                {status === 'otp' && (
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Typography component="h1" variant="h5" sx={{ marginBottom: '20px' }}  >
+                            Logga in
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '17px' }}>
+                            Fyll i kod som skickates till
+                        </Typography>
+                        <Typography variant="body1" component="p" sx={{ fontSize: '17px', color: "red" }}>
+
+                        </Typography>
+                        <div className="container">
+                            <input type="text" className="input-box" value={0} />
+                            <input type="text" className="input-box" value={0} />
+                            <input type="text" className="input-box" value={0} />
+                            <input type="text" className="input-box" value={0} />
+                            <input type="text" className="input-box" value={0} />
+                            <input type="text" className="input-box" value={0} />
+                        </div>
+                        <Box component="form" onSubmit={handleOtpSubmit} noValidate sx={{ mt: 1 }} style={{ marginTop: "15px" }}>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, backgroundColor: '#9f9f9f', color: 'white', padding: '8px', width: '300px' }}
+                                style={{ fontSize: "17px", display: 'flex' }}
+                            >
+                                Logga in
+                            </Button>
+                            <Link href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+                                Visit Example.com
                             </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
+                        </Box>
+                        <Box style={{ marginTop: "40px" }}>
+                            <img src={myImage} className="image-style" alt="Description of the image" />
+                            <Typography variant="body1" component="p" sx={{ fontSize: '20px', padding: "20px  " }}>
+                                Har du Fragor om service eller verkstad?
+                            </Typography>
+                        </Box>
+                    </Box>
+                )}
+            </div>
+
         </Container>
     );
 }
